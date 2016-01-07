51c2ef9e77eac979896c1239561b87524be62649
Xenofon Spafaridis <nohponex@gmail.com>
2016-01-07 23:40:05 +0200

    Update README

--- tests/APP/index.php
+++ tests/APP/index.php
@@ -8,7 +8,7 @@ $settings = \Phramework\SystemLog\APP\Bootstrap::getSettings();
 
 $phramework = \Phramework\SystemLog\APP\Bootstrap::prepare(true);
 
-$settings['system-log']->log = 'Phramework\\SystemLog\\Log\\TerminalLog';
+//$settings['system-log']->log = 'Phramework\\SystemLog\\Log\\TerminalLog';
 
 //$settings['system-log']->log = 'Phramework\\SystemLog\\Log\\DatabaseLog';
 
